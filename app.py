a19c6962b337a941edf1959fc19cdc03855cd1a6
kumagai-nkt <kumagaitakuya0423@gmail.com>
2023-05-19 15:41:11 +0900

    commit

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def index():
     if request.method == "POST":
         animal = request.form["animal"]
         response = openai.Completion.create(
+            # model="gpt-3.5-turbo",
             model="text-davinci-003",
             prompt=generate_prompt(animal),
             temperature=0.6,
